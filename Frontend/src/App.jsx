a81b9bd5968eff19e978915667ee46d3a6a6da6c
Gokul <gokul@saaragh.in>
2023-09-30 16:32:59 +0530

    Image animation creation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+
 function App() {
   return (
     <>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <Header />
+        <Profile />
+      </div>
     </>
   );
 }
