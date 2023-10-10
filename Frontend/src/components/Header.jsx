24164c4eae66cadae176298b6805ddbf4f9ce3c9
gokul <gokulchandan18@gmail.com>
2023-10-10 23:43:20 +0530

    fixes

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -4,7 +4,6 @@ import WorkExperience from "./WorkExperience";
 import Skills from "./Skills";
 import EducationProjects from "./EducationProjects";
 import ContactMe from "./ContactMe";
-import DropDownMenu from "./DropDownMenu";
 
 const Header = () => {
   const [tab, setTab] = useState("Home");
